3e21011ee4fc0c2b569e4ef3b5dcfa2886253ea2
Roger Walker <roger.john.walker@gmail.com>
2019-02-12 22:45:28 -0500

    Timed out... maybe try this in a fatser language?

--- javascript/5kyu/best-travel.js
+++ javascript/5kyu/best-travel.js
@@ -1,7 +1,12 @@
 function chooseBestSum(t, k, ls) {
+
+
     if (ls.length < k) return null;
+    ls.sort( (a, b) => b - a );
+    console.log("t: " + t + " k: " + k + " ls: " + ls);
     var results = recChooseBestSum(t, k, ls, 0, [])
-    return results.reduce( (acc, cur) => cur > acc ? cur : acc );
+
+    return results.length > 0 ? results.reduce( (acc, cur) => cur > acc ? cur : acc ) : null;
 
 }
 
@@ -13,12 +18,25 @@ function recChooseBestSum(t, k, ls, curr, results){
     }
     return results;
   }
+
   for(var i = 0; i < ls.length; i++){
+    if (curr+ls[i] <= t)
+    {
+      let new_ls = ls.slice();
+      new_ls.splice(i,1);
 
-    let new_ls = ls.slice();
-    new_ls.splice(i,1);
+      // attempt to skip  meaningless branches with large k
+      // nope ... made it worse...
+      /*
+      if (results.length > 0 && new_ls.length > 0){
+        let max = results.reduce( (a, b) => Math.max(a,b) );
+        let sum = new_ls.reduce( (a, c) => a + c );
+        if (curr + sum < max ) continue;
+      }
+      */
 
-    results = recChooseBestSum(t, k-1, new_ls, curr+ls[i], results);
+      results = recChooseBestSum(t, k-1, new_ls, curr+ls[i], results);
+    }
   }
   return results;
 }
